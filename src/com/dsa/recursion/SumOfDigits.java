8fe10b3b9a91a4120f45d3b7d7ca5d4ab5b47fe4
brijesh <brijesh@192.168.43.78>
2023-01-09 19:51:00 +0530

    Sum of digits code commit

--- src/com/dsa/recursion/SumOfDigits.java
+++ src/com/dsa/recursion/SumOfDigits.java
@@ -1,10 +1,18 @@
 package com.dsa.recursion;
 
 public class SumOfDigits {
-
+	
+	public static int SOD(int n) {
+		if(n==0 || n<0) {
+			return 0;
+		}
+		return n%10+SOD(n/10);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int sum = SumOfDigits.SOD(1111);
+		System.out.println("Sum of digits = "+sum);
 	}
 
 }
